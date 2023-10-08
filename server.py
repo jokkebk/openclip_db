3bb2e2bd8cb594aa919de4c31cf68b641a91777a
Joonas Pihlajamaa <joonas.pihlajamaa@gmail.com>
2023-10-08 15:45:42 +0300

    Working image search

--- server.py
+++ server.py
@@ -4,7 +4,35 @@
     and additionally have /api/ endpoints for the web app to
     fetch data from the database."""
 
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, send_file
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder='static')
+CORS(app)
+
+from db import ImageEmbeddings, get_session
+
+session = get_session('images.db')
+
+# Fetch all image embeddings from database along with their filenames and ids
+data = session.query(ImageEmbeddings.id, ImageEmbeddings.filename, ImageEmbeddings.embedding).all()
+
+import numpy as np
+
+# Create np.array of embeddings
+embeddings = np.array([d[2] for d in data])
+
+# Discard the embeddings from the data to save some memory
+data = [(d[0], d[1]) for d in data]
+
+# Show how much memory the embeddings take up
+print(len(embeddings), "embeddings take up", embeddings.nbytes / 1024 / 1024, "MB")
+
+import torch, open_clip
+#from PIL import Image
+
+model, _, preprocess = open_clip.create_model_and_transforms('ViT-H-14', pretrained='laion2b_s32b_b79k')
+tokenizer = open_clip.get_tokenizer('ViT-H-14')
 
 # Serve the static Svelte app
 @app.route('/')
@@ -15,34 +43,59 @@ def index():
 def static_file(path):
     return send_from_directory('static', path)
 
-# JSON API for fetching news articles
 @app.route('/api/search', methods=['GET'])
 def search():
-    query_string = request.args.get('query', '')
+    """Search for images similar to the query string."""
+    query_string = request.args.get('q', '')
+    
+    with torch.no_grad(), torch.cuda.amp.autocast():
+        text = tokenizer(query_string)
+        text_features = model.encode_text(text)
+        # Normalize, though it isn't strictly necessary
+        text_features /= text_features.norm(dim=-1, keepdim=True)
+        print(query_string, text_features.shape)
 
-    # Create embedding for query string
-    query_embedding = model.encode(query_string)
+    # Return text features as JSON response
+    #response = jsonify({'results': text_features.cpu().numpy().tolist()})
 
     # Calculate cosine similarity between query embedding and all embeddings
-    scores = query_embedding @ embeddings.T
+    scores = text_features @ embeddings.T
+    print(scores.shape)
+    
+    # Convert to Python list
+    scores = scores.squeeze().cpu().numpy().tolist()
+    print(len(scores))
 
     # Sort the entries by score
-    sorted_entries = sorted(zip(scores, entries), reverse=True)
+    sorted_entries = sorted(zip(scores, data), reverse=True)
 
     # Return the top 10 entries
     results = []
-    for score, entry in sorted_entries[:10]:
+    for score, (id, filename) in sorted_entries[:10]:
         results.append({
-            'title': entry.title,
-            'content': entry.summary,
-            'url': entry.url,
-            'published_date': entry.published_date.isoformat(),
+            'id': id,
+            'filename': filename,
             'score': float(score),
-            'read': entry.read,
-            'rating': entry.rating,
         })
 
-    return jsonify({'entries': results})
+    response = jsonify({'results': results})
+
+    # Accept CORS requests from any origin
+    #response.headers.add('Access-Control-Allow-Origin', '*')
+
+    # For now, just echo back the query string
+    return response
+
+@app.route('/api/image/<int:id>', methods=['GET'])
+def image(id):
+    """Return the image with the given id."""
+    # Get the filename of the image with the given id from data
+    filename = next((d[1] for d in data if int(d[0]) == id), None)
+    if filename is None:
+        return jsonify({'error': 'Image not found'}), 404
+
+    # Return the image file
+    return send_file(filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- svelte-ui/src/App.svelte
+++ svelte-ui/src/App.svelte
@@ -1,10 +1,45 @@
 <script>
-  //import svelteLogo from './assets/svelte.svg'
-  //import viteLogo from '/vite.svg'
-  //import Counter from './lib/Counter.svelte'
+  let query = '';
+  let results = [];
+  
+  const endpoint = 'http://localhost:5000';
+
+  function search() {
+    console.log(query);
+    // Run a search query against endpoint's /api/search
+    fetch(`${endpoint}/api/search?q=${query}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // Return results if there are any
+        if (data.results) results = data.results;
+      });
+  }
 </script>
 
+<style>
+/* Limit image size to 256x256 */
+img.result {
+  max-width: 256px;
+  max-height: 256px;
+}
+</style>
 <!-- <main> -->
 <h1>OpenCLIP DB Svelte GUI</h1>
 
-<p>It's working.</p>
+<p>
+  Query: <input id="query" bind:value={query} />
+<button on:click={search}>Search</button>
+</p>
+
+{#if results.length > 0}
+  <ul>
+    {#each results as r}
+      <li>#{r.id}: Score {r.score}
+      <img class="result" src={`${endpoint}/api/image/${r.id}`} alt={r.filename}/>
+      </li>
+    {/each}
+  </ul>
+{:else}
+  <p>No results</p>
+{/if}
